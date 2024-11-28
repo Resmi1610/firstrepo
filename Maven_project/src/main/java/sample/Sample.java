2e80612169e66f93f0939467ed3556353957021d
resmi <resmi@LAPTOP-MM2SMBKA>
2024-11-28 21:07:30 +0530

    Initial commit

---
+++ Maven_project/src/main/java/sample/Sample.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		int a=10;
+		int b=15;
+		int c=a+b;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
